0dc76905054bc79ca66cda55f26dbdb329565cae
Wesley Martins <wesleymartins.dev@outlook.com>
2023-11-13 21:22:09 -0300

    Project created

---
+++ src/main/java/com/wesleymartins/infogames/InfogamesApplication.java
@@ -0,0 +1,13 @@
+package com.wesleymartins.infogames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfogamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfogamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wesleymartins/infogames/InfogamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesleymartins.infogames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfogamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
