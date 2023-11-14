91c6edaa1317986c7355a7508f95dd7370dbc545
Wesley Martins <wesleymartins.dev@outlook.com>
2023-11-13 21:53:53 -0300

    Game seed

--- src/main/java/com/wesleymartins/infogames/entities/Game.java
+++ src/main/java/com/wesleymartins/infogames/entities/Game.java
@@ -14,21 +14,25 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String plataform;
+    private String platforms;
+    private Double score;
+
     private String imgUrl;
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){
 
     }
 
-    public Game(Long id, String title, Integer year, String genre, String plataform, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.plataform = plataform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -65,13 +69,19 @@ public class Game {
     public void setGenre(String genre) {
         this.genre = genre;
     }
-
-    public String getPlataform() {
-        return plataform;
+    public Double getScore() {
+        return score;
     }
 
-    public void setPlataform(String plataform) {
-        this.plataform = plataform;
+    public void setScore(Double score) {
+        this.score = score;
+    }
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
 
     public String getImgUrl() {
